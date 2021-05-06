1126ef5e4b7f7933ba76747fd23c57fd4a514262
Aino Kinnunen <ainokinnunen@srslymackinnunen.home>
2021-05-06 23:50:17 +0300

    Changed design, added loading state, fixed bugs
    Changed styling of the page, added gradiant colors, changed font, added loading state when no city known, fixed get current location to be automatic

--- src/app.js
+++ src/app.js
@@ -1,34 +1,41 @@
 let apiKey = "e9e91b2ff3ef4ee9f513c835d919042a";
 let temperatureElement = document.querySelector("#temperature");
-let heading = document.querySelector("h1");
+let heading = document.querySelector("#city");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let weatherdescriptionElement = document.querySelector("#description");
 let weathericonElement = document.querySelector("#weather-icon");
 let temperatureFahrenheit;
+let weatherAppLoading = document.querySelector(".weather-app-loading");
+let weatherAppLoaded = document.querySelector(".weather-app-loaded");
 let temperatureFahrenheitElement = document.querySelector("#fahrenheit");
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   temperatureFahrenheit = (Math.round(response.data.main.temp) / 5) * 9 + 32;
   let position = input.value || response.data.name;
   temperatureElement.innerHTML = `${temperature}`;
-  temperatureFahrenheitElement.innerHTML = `${temperatureFahrenheit}`;
+  temperatureFahrenheitElement.innerHTML = `${temperatureFahrenheit.toFixed(
+    0
+  )}`;
   heading.innerHTML = input.value || response.data.name;
   humidityElement.innerHTML = `${response.data.main.humidity}`;
   windElement.innerHTML = `${response.data.wind.speed}`;
   weatherdescriptionElement.innerHTML = `${response.data.weather[0].main}`;
   weathericonElement.src =
     "http://openweathermap.org/img/w/" + response.data.weather[0].icon + ".png";
-  console.log(response.data);
+  weatherAppLoading.style.display = "none";
+  weatherAppLoaded.style.display = "block";
 }
 function showPosition(position) {
   let apiUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
-    input.value +
+    position +
     "&appid=" +
     apiKey +
     "&units=metric";
   axios.get(apiUrl).then(showTemperature);
+  weatherAppLoading.style.display = "block";
+  weatherAppLoaded.style.display = "none";
 }
 function showCurrentPosition(position) {
   let apiUrl =
@@ -67,10 +74,13 @@ let updateCity = function () {
   showPosition(input.value);
 };
 
-let updateCurrentCity = function () {
-  temperatureElement.innerHTML = "We're working on your request";
-  input.value = null;
-  navigator.geolocation.getCurrentPosition(showCurrentPosition);
-};
+navigator.geolocation.getCurrentPosition(showCurrentPosition);
 button.addEventListener("click", updateCity);
 currentLocationButton.addEventListener("click", updateCurrentCity);
+
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    showPosition(input.value);
+  }
+});
