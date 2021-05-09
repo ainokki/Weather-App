54ad38938a392ef4e0759bbdd98fed4999f941c9
Aino Kinnunen <ainokinnunen@srslymackinnunen.home>
2021-05-10 02:31:27 +0300

    Forecast, styling changes
    Added JS forecast function, changed styling, fixed bugs§

--- src/app.js
+++ src/app.js
@@ -9,6 +9,28 @@ let temperatureFahrenheit;
 let weatherAppLoading = document.querySelector(".weather-app-loading");
 let weatherAppLoaded = document.querySelector(".weather-app-loaded");
 let temperatureFahrenheitElement = document.querySelector("#fahrenheit");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let formatDate = function (timezoneOffset) {
+  let now = new Date();
+  now = new Date(now.getTime() + timezoneOffset);
+  let day = days[now.getDay()];
+  let number = now.getDate();
+  let month = now.getMonth() + 1;
+  let year = now.getFullYear();
+  let minutes = now.getMinutes();
+  let hour = now.getHours();
+  return `${day} ${number}/${month}/${year} `;
+};
+
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   temperatureFahrenheit = (Math.round(response.data.main.temp) / 5) * 9 + 32;
@@ -25,37 +47,61 @@ function showTemperature(response) {
     "http://openweathermap.org/img/w/" + response.data.weather[0].icon + ".png";
   weatherAppLoading.style.display = "none";
   weatherAppLoaded.style.display = "block";
+  time.innerHTML = formatDate(response.data.timezone);
+
+  getForecast(response.data.coord);
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#weather-forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Today", "Tomorrow", "Sunday", "Monday", "Tuesday"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-    <div class="row">
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
             <div class="weather-forecast-date">
-                ${day}<img
-                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                ${formatDay(forecastDay.dt)}<img
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""
-                  width="36"
+                  width="48"
                 />
             </div>
             <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-temperatures-max"> 18°</span>
-                <span class="weather-forecast-temperatures-min">12°</span>
+                <span class="weather-forecast-temperatures-max"> ${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="weather-forecast-temperatures-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
             </div>
         </div>
-    </div>
           `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "e9e91b2ff3ef4ee9f513c835d919042a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showPosition(position) {
@@ -84,25 +130,9 @@ let button = document.querySelector("#searchCityButton");
 let currentLocationButton = document.querySelector("#currentLocationButton");
 let time = document.querySelector(".time");
 let input = document.querySelector("#city-input");
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let minutes = now.getMinutes();
-let hour = now.getHours();
-let formatDate = function (now) {
-  return `${day} ${hour} ${minutes} `;
-};
+
 let updateCity = function () {
   temperatureElement.innerHTML = "We're working on your request";
-  time.innerHTML = formatDate(now);
   showPosition(input.value);
 };
 
@@ -119,5 +149,3 @@ input.addEventListener("keyup", function (event) {
     showPosition(input.value);
   }
 });
-
-displayForecast();
